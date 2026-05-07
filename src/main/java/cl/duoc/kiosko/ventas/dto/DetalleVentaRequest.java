121d457e3f97f9abbedf72eb466d337f7adec9a2
erick <er.rosales@duocuc.cl>
2026-05-07 17:01:32 -0400

    Eighth commit | se agrega el spring security que de momento solo debe decir lo que hace cada service

--- src/main/java/cl/duoc/kiosko/ventas/dto/DetalleVentaRequest.java
+++ src/main/java/cl/duoc/kiosko/ventas/dto/DetalleVentaRequest.java
@@ -1,5 +1,7 @@
 package cl.duoc.kiosko.ventas.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +11,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class DetalleVentaRequest {
     //NO VA ID PORQUE EL USUARIO NO PONE ID AL CREARLO
+    @NotNull(message = "Debe existir un id producto")
     private Long productoId;
+    @NotNull(message = "La cantidad es obligatoria")
+    @Min(value = 0, message = "La cantidad no puede ser negativa")
     private int cantidad;
+    @NotNull
+    @Min(value = 0, message = "El subtotal no puede ser negativo")
     private int subtotal;
 
 }
