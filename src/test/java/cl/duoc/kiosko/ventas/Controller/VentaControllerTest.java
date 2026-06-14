bae98b9865b14a2f8f9917a0812ba29b52cca995
erick <er.rosales@duocuc.cl>
2026-06-13 21:22:21 -0400

    funcionan los test (Por fin) y se implemento JACOCO

--- src/test/java/cl/duoc/kiosko/ventas/Controller/VentaControllerTest.java
+++ src/test/java/cl/duoc/kiosko/ventas/Controller/VentaControllerTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -22,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 // 🚀 @WebMvcTest levanta SOLO el controlador, haciendo la prueba súper rápida
 @WebMvcTest(VentaController.class)
+@ActiveProfiles("test")
 class VentaControllerTest {
 
     @Autowired
--- src/test/java/cl/duoc/kiosko/ventas/Service/VentaServiceTest.java
+++ src/test/java/cl/duoc/kiosko/ventas/Service/VentaServiceTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,6 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
+@ActiveProfiles("test")
 class VentaServiceTest {
 
     @Mock
