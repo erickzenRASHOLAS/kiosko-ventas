d472b02d34382935570327454a781b72ff42fec4
erick <er.rosales@duocuc.cl>
2026-06-16 15:20:45 -0400

    CARGA EL SWAGGER MEDIANTE RAILWAY falta probar los otros metodos (ya no da error CORS)

--- src/main/java/cl/duoc/kiosko/ventas/Config/SwaggerConfig.java
+++ src/main/java/cl/duoc/kiosko/ventas/Config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package cl.duoc.kiosko.ventas.Config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +10,17 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
     @Bean// llama componentes externos (funciona como libreria)
     public OpenAPI customerOpenAPI(){
+
+        // Configuración del servidor de producción con HTTPS (railway)
+        Server productionServer = new Server();
+        productionServer.setUrl("https://kiosko-ventas-production.up.railway.app");
+        productionServer.setDescription("Servidor de Producción en Railway");
+
+        // Configuración para probar en local
+        Server localServer = new Server();
+        localServer.setUrl("http://localhost:8082");
+        localServer.setDescription("Servidor de Desarrollo Local");
+
         return new OpenAPI()
                 .info(new Info()
                         .title("API 2026 Ventas kiosko")
