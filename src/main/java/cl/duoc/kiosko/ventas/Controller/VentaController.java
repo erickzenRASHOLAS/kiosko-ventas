c5a77edf950f669e8864d2abbb77bf3727ff2d08
Erick Parra <er.rosales@duocuc.cl>
2026-06-25 12:27:13 -0400

    se corrige el url del controller de ventas

--- src/main/java/cl/duoc/kiosko/ventas/Controller/VentaController.java
+++ src/main/java/cl/duoc/kiosko/ventas/Controller/VentaController.java
@@ -21,7 +21,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
-@RequestMapping("/v2/ventas")
+@RequestMapping("/v1/ventas")
 @Tag(name="Ventas", description = "Operaciones relacionadas con las ventas")
 public class VentaController {
     @Autowired
